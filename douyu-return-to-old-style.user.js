71c29e50fdfe3df4c6cc7ae3ed4336c443d2cd38
chunchundeniuma <130929498+chunchundeniuma@users.noreply.github.com>
2025-12-22 14:35:27 +0800

    update: deprecated "douyu-return-to-old-style"

--- douyu-return-to-old-style.user.js
+++ douyu-return-to-old-style.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://www.github.com/chunchundeniuma/userscripts
 // @match        *://*.douyu.com/*
 // @grant        none
-// @version      1.2.0
+// @version      1.2.1
 // @author       chunchundeniuma
 // @description  返回旧版 web 页面
 // @run-at       document-start
@@ -13,6 +13,8 @@
 // @supportURL   https://www.github.com/chunchundeniuma/userscripts/issues
 // ==/UserScript==
 
+
+//2025/12/22 add: douyu 移除了旧版 url，以前的 beta 版网页替代了旧版网页，此脚本已失效。
 (function () {
     "use strict";
 
